1faf7b068c5226b0585e5d1b95775c9da997007d
Andrew Spoda <saaadel@users.noreply.github.com>
2016-12-25 21:10:57 +0300

    Update pkg-manager-which.sh

--- linux/pkg-manager-which.sh
+++ linux/pkg-manager-which.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 ## Description: Test for yum or apt based linux
 
+## HOW TO USE:
+# named_pipe=`cat /dev/urandom | env LC_CTYPE=C tr -cd 'a-f0-9' | head -c 32` && mkfifo $named_pipe && (curl -sjkL https://raw.githubusercontent.com/saaadel/scripts/master/linux/pkg-manager-which.sh > $named_pipe &) && . /dev/stdin < $named_pipe && rm -f $named_pipe
+
+
 export IS_YUM_PKG_MANAGER=`which yum &>/dev/null && echo 1 || echo ''`
 export IS_APT_GET_PKG_MANAGER=`which apt-get &>/dev/null && echo 1 || echo ''`
-export PKG_MANAGER=$( which -v yum || which apt-get ) || exit 1
+export PKG_MANAGER=$( which yum || which apt-get ) || exit 1
