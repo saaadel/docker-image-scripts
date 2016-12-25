65ba69afd7131414c2406a22b703a80668dc4a7a
Andrew Spoda <saaadel@users.noreply.github.com>
2016-12-25 18:37:12 +0300

    Create clean-update-install.sh
    Do clean update, and install packages with apt-get, without any docs. Clear the cache at finish.

---
+++ linux/apt-get/clean-update-install.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+## Description: Do clean update, and install packages with apt-get, without any docs. Clear the cache at finish.
+
+## HOW TO USE (as root):
+# curl https://raw.githubusercontent.com/saaadel/scripts/master/linux/apt-get/clean-update-install.sh | bash /dev/stdin [package1 [package2...]]
+
+
+apt-get update && \
+apt-get --no-install-recommends install -y curl sudo which $* && \
+rm -rf /var/lib/apt/lists/*
