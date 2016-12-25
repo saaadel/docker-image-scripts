fccdae1aa523a2b8a0cac684ab6fe544edc6fd10
Andrew Spoda <saaadel@users.noreply.github.com>
2016-12-25 21:25:47 +0300

    Update clean-update-install.sh

--- linux/yum/clean-update-install.sh
+++ linux/yum/clean-update-install.sh
@@ -1,8 +1,8 @@
-#!/bin/bash
+#!/bin/sh
 ## Description: Do clean update, and install packages with yum, without any docs. Clear the cache at finish.
 
 ## HOW TO USE (as root):
-# curl https://raw.githubusercontent.com/saaadel/scripts/master/linux/yum/clean-update-install.sh | bash /dev/stdin [package1 [package2...]]
+# curl https://raw.githubusercontent.com/saaadel/scripts/master/linux/yum/clean-update-install.sh | sh /dev/stdin [package1 [package2...]]
 
 
 yum clean all && \
