4d3a6ca3cba10dc2b1b7addc0267a3339d2fa5c1
Andrew Spoda <saaadel@users.noreply.github.com>
2016-12-25 18:45:05 +0300

    Create maven.sh
    Install Maven

---
+++ linux/install/maven.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+## Description: Install Maven.
+
+## HOW TO USE (as root):
+# curl https://raw.githubusercontent.com/saaadel/scripts/master/linux/install/maven.sh | bash /dev/stdin
+## OR
+# bundle=maven3 bash -c 'curl https://raw.githubusercontent.com/saaadel/scripts/master/linux/install/maven.sh | bash /dev/stdin'
+
+
+maven3=http://www-us.apache.org/dist/maven/maven-3/3.3.9/binaries/apache-maven-3.3.9-bin.tar.gz
+
+[ -z $bundle ] && bundle='maven3'
+[ -z $bundle_url ] && bundle_url=$(eval echo "\$${bundle}")
+
+env_sh_filepath=/etc/profile.d/maven-env.sh
+env_csh_filepath=/etc/profile.d/maven-env.csh
+
+yum install -y --setopt=tsflags=nodocs unzip
+yum clean all
+rm -rf /var/cache/yum/*
+
+rm -rf /tmp/curl.tmp
+curl -sjkLo /tmp/curl.tmp "${bundle_url}"
+
+## tar.gz with one directory only inside
+dirname=`zipinfo -1 /tmp/curl.tmp | egrep '^[^/]+/?$' | tail -1 | sed 's:/*$::'`
+
+unzip -oqq /tmp/curl.tmp -d /opt
+rm -rf /tmp/curl.tmp
+
+export M2_HOME=/opt/${dirname}
+export PATH=$PATH:$M2_HOME/bin
+/bin/echo -e "\nexport M2_HOME=$M2_HOME\n" >> $env_sh_filepath
+/bin/echo -e "\nsetenv M2_HOME \"$M2_HOME\"\n" >> $env_csh_filepath
+
+/bin/echo -e "\nexport PATH=\$PATH:$M2_HOME/bin\n" >> $env_sh_filepath
+/bin/echo -e "\nsetenv PATH \"\$PATH:$M2_HOME/bin\"\n" >> $env_csh_filepath
+
+chmod 644 $env_sh_filepath $env_csh_filepath
