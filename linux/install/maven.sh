555784419dd40c1295872d9214e19d9fe216c3e8
Andrew Spoda <saaadel@users.noreply.github.com>
2016-12-26 10:34:36 +0300

    Update maven.sh

--- linux/install/maven.sh
+++ linux/install/maven.sh
@@ -20,7 +20,7 @@ rm -rf /tmp/curl.tmp
 curl -sjkLo /tmp/curl.tmp "${bundle_url}"
 
 ## tar.gz with one directory only inside
-dirname=`tar -ztf /tmp/curl.tmp | egrep '^[^/]+/?$' | tail -1 | sed 's:/*$::'`
+dirname=`tar -ztf /tmp/curl.tmp | awk -F/ '{print $1}' | uniq | tail -1 | sed 's:/*$::'`
 
 tar -xzf /tmp/curl.tmp -C /opt
 rm -rf /tmp/curl.tmp
