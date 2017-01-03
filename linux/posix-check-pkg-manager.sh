4591d68e9549bf0e8c6533157c470cd0b6d89025
Andrew Spoda <saaadel@users.noreply.github.com>
2017-01-03 13:19:06 +0300

    Update posix-check-pkg-manager.sh

--- linux/posix-check-pkg-manager.sh
+++ linux/posix-check-pkg-manager.sh
@@ -5,6 +5,6 @@
 # named_pipe=`cat /dev/urandom | env LC_CTYPE=C tr -cd 'a-f0-9' | head -c 32` && mkfifo $named_pipe && (curl -sjkL https://raw.githubusercontent.com/saaadel/scripts/master/linux/posix-check-pkg-manager.sh > $named_pipe &) && . /dev/stdin < $named_pipe && rm -f $named_pipe
 
 
-export IS_YUM_PKG_MANAGER=`which yum &>/dev/null && echo 1 || echo ''`
-export IS_APT_GET_PKG_MANAGER=`which apt-get &>/dev/null && echo 1 || echo ''`
+export IS_YUM_PKG_MANAGER=`which yum 1>/dev/null 2>/dev/null && echo 1 || echo ''`
+export IS_APT_GET_PKG_MANAGER=`which apt-get 1>/dev/null 2>/dev/null && echo 1 || echo ''`
 export PKG_MANAGER=$( which yum || which apt-get ) || exit 1
