8a50b4fd57c3b9e9b374562598b7a7b63d260a4a
Andrew Spoda <saaadel@users.noreply.github.com>
2017-01-05 16:21:36 +0300

    Update nodejs.sh

--- linux/apt-get/nodejs.sh
+++ linux/apt-get/nodejs.sh
@@ -5,5 +5,5 @@
 # curl -jkL https://raw.githubusercontent.com/saaadel/scripts/master/linux/apt-get/nodejs.sh | sh /dev/stdin
 
 apt-get update && \
-apt-get --no-install-recommends install -y git subversion nodejs npm
+DEBIAN_FRONTEND=noninteractive apt-get --no-install-recommends -yq install git subversion nodejs npm && \
 rm -rf /var/lib/apt/lists/*
