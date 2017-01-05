e5c7ece88eb9494bc98ec2c37e9d98697b6d8810
Andrew Spoda <saaadel@users.noreply.github.com>
2017-01-05 16:22:52 +0300

    Update nodejs.sh

--- linux/yum/nodejs.sh
+++ linux/yum/nodejs.sh
@@ -13,8 +13,9 @@ nodejs7=https://rpm.nodesource.com/setup_7.x
 [ -z "$bundle_url" ] && bundle_url=$(eval echo "\$${bundle}")
 
 curl -jkL "${bundle_url}" | bash -
+
 yum clean all && \
 yum update -y && \
-yum install -y --setopt=tsflags=nodocs git subversion nodejs npm
-yum clean all
+yum -y --setopt=tsflags=nodocs install git subversion nodejs npm && \
+yum clean all && \
 rm -rf /var/cache/yum/*
