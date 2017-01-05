92c8805c327a9cfe123877ca5ebf0ac7b4e22f0b
Andrew Spoda <saaadel@users.noreply.github.com>
2017-01-05 16:20:51 +0300

    Update clean-update-install.sh

--- linux/apt-get/clean-update-install.sh
+++ linux/apt-get/clean-update-install.sh
@@ -5,7 +5,7 @@
 # curl -jkL https://raw.githubusercontent.com/saaadel/scripts/master/linux/apt-get/clean-update-install.sh | sh /dev/stdin [package1 [package2...]]
 
 
-apt-get update
-DEBIAN_FRONTEND=noninteractive apt-get --no-install-recommends install -yq which
-DEBIAN_FRONTEND=noninteractive apt-get --no-install-recommends install -yq curl $*
+apt-get update && \
+( DEBIAN_FRONTEND=noninteractive apt-get --no-install-recommends -yq install which || true ) && \
+DEBIAN_FRONTEND=noninteractive apt-get --no-install-recommends -yq install curl $* && \
 rm -rf /var/lib/apt/lists/*
