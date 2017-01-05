9da759fec4ac18f856391171cb8c7f4b9468e09a
Andrew Spoda <saaadel@users.noreply.github.com>
2017-01-05 14:20:58 +0300

    Update bash-clean-update-install.sh

--- linux/bash-clean-update-install.sh
+++ linux/bash-clean-update-install.sh
@@ -2,14 +2,14 @@
 ## Description: Test for yum or apt based linux, and install packages (for BaSH)
 
 ## HOW TO USE:
-# curl -sjkL https://raw.githubusercontent.com/saaadel/scripts/master/linux/bash-clean-update-install.sh | bash /dev/stdin [package1 [package2...]]
+# curl -jkL https://raw.githubusercontent.com/saaadel/scripts/master/linux/bash-clean-update-install.sh | bash /dev/stdin [package1 [package2...]]
 
-. <( curl -sjkL https://raw.githubusercontent.com/saaadel/scripts/master/linux/bash-check-pkg-manager.sh )
+. <( curl -jkL https://raw.githubusercontent.com/saaadel/scripts/master/linux/bash-check-pkg-manager.sh )
 
 if [[ -n "$IS_YUM_PKG_MANAGER" ]]; then
-    curl -sjkL https://raw.githubusercontent.com/saaadel/scripts/master/linux/yum/clean-update-install.sh | bash /dev/stdin $*
+    curl -jkL https://raw.githubusercontent.com/saaadel/scripts/master/linux/yum/clean-update-install.sh | bash /dev/stdin $*
 else
     if [[ -n "$IS_APT_GET_PKG_MANAGER" ]]; then
-        curl -sjkL https://raw.githubusercontent.com/saaadel/scripts/master/linux/apt-get/clean-update-install.sh | bash /dev/stdin $*
+        curl -jkL https://raw.githubusercontent.com/saaadel/scripts/master/linux/apt-get/clean-update-install.sh | bash /dev/stdin $*
     fi
 fi
