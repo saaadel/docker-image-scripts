937e85c3de364a8891285ea696db09504bc7d1f2
Andrew Spoda <saaadel@users.noreply.github.com>
2017-01-05 16:40:34 +0300

    Update clean-update-install.sh

--- linux/yum/clean-update-install.sh
+++ linux/yum/clean-update-install.sh
@@ -7,7 +7,7 @@
 
 yum clean all && \
 yum update -y && \
-( yum -y --setopt=tsflags=nodocs install which || true ) && \
+( yum -y --setopt=tsflags=nodocs install which 2>/dev/null || true ) && \
 yum -y --setopt=tsflags=nodocs install curl $* && \
 yum clean all && \
 rm -rf /var/cache/yum/*
