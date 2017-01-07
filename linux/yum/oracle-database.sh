dd3e3f583e6f716771e09e8f20cf8450a60e8438
Andrew Spoda <saaadel@users.noreply.github.com>
2017-01-07 19:17:22 +0300

    Update oracle-database.sh

--- linux/yum/oracle-database.sh
+++ linux/yum/oracle-database.sh
@@ -15,6 +15,9 @@ Enterprise_Edition_Database_12c_2of2=https://edelivery.oracle.com/akam/otn/linux
 [ -z "$bundle1_url" ] && bundle1_url=$(eval echo "\$${bundle}_1of2")
 [ -z "$bundle2_url" ] && bundle2_url=$(eval echo "\$${bundle}_2of2")
 
+env_sh_filepath=/etc/profile.d/oracle-db-env.sh
+env_csh_filepath=/etc/profile.d/oracle-db-env.csh
+
 rm -rf /tmp/curl.tmp
 curl -jkLo /tmp/curl.tmp -H "Cookie: oraclelicense=accept-database_111060_linx8664-cookie; OHS-edelivery.oracle.com-443=2A1418E7B23D34F28CCD775F9A3D40ED459B42D5D358D81EBC9A91395FB9587BD328E972B61EB5905A29C63693BFB92F34F369682804D3E5A35091A6EA3882C6A32FA506D7B2C8BB9224E931DDDDE4A3E456E7D70124568E378D7722FA9C1919F20D38D29D12284625B83B6111413EB682075BF2442D783EC6F6CADA380F4BE089312701B47BACA77845610749E9A944990061299C292845E9AD115C4F52821250D9DA3812CC48C6FE71C6225A7E52230C3A641618905E9D9AB9F538BC33E1EF174730E02084DAB060BF6ADF2EA24CC8E5DFCA03277F29EDD003CE989B07B443D851848B437650B3AA9E0A360CE5F6BA7058C3A35BD7F366~" "${bundle1_url}"
 
@@ -36,11 +39,24 @@ yum -y install oracle-rdbms-server-12cR1-preinstall unzip wget tar openssl
 yum clean all
 
 export ORACLE_BASE=/opt/oracle
+/bin/echo -e "\nexport ORACLE_BASE=$ORACLE_BASE\n" > $env_sh_filepath
+/bin/echo -e "\nsetenv ORACLE_BASE \"$ORACLE_BASE\"\n" > $env_csh_filepath
+
 export ORACLE_HOME=/opt/oracle/product/12.1.0.2/dbhome_1
+/bin/echo -e "\nexport ORACLE_HOME=$ORACLE_HOME\n" > $env_sh_filepath
+/bin/echo -e "\nsetenv ORACLE_HOME \"$ORACLE_HOME\"\n" > $env_csh_filepath
 
 export PATH=$ORACLE_HOME/bin:$ORACLE_HOME/OPatch/:/usr/sbin:$PATH
+/bin/echo -e "\nexport PATH=\$ORACLE_HOME/bin:\$ORACLE_HOME/OPatch/:/usr/sbin:\$PATH\n" > $env_sh_filepath
+/bin/echo -e "\nsetenv PATH \"\$ORACLE_HOME/bin:\$ORACLE_HOME/OPatch/:/usr/sbin:\$PATH\"\n" > $env_csh_filepath
+
 export LD_LIBRARY_PATH=$ORACLE_HOME/lib:/usr/lib
+/bin/echo -e "\nexport LD_LIBRARY_PATH=\$ORACLE_HOME/lib:/usr/lib\n" > $env_sh_filepath
+/bin/echo -e "\nsetenv LD_LIBRARY_PATH \"\$ORACLE_HOME/lib:/usr/lib\"\n" > $env_csh_filepath
+
 export CLASSPATH=$ORACLE_HOME/jlib:$ORACLE_HOME/rdbms/jlib
+/bin/echo -e "\nexport CLASSPATH=\$ORACLE_HOME/jlib:\$ORACLE_HOME/rdbms/jlib\n" > $env_sh_filepath
+/bin/echo -e "\nsetenv CLASSPATH \"\$ORACLE_HOME/jlib:\$ORACLE_HOME/rdbms/jlib\"\n" > $env_csh_filepath
 
 mkdir -p $ORACLE_BASE/oradata
 groupadd -g 500 dba
